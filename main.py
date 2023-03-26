acf29b9314ee1a095882edb5f3c97a446f76ad1f
Near8954 <roma.baldin@gmail.com>
2023-03-26 21:43:30 +0500

    pre relise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5 import uic  # Импортируем uic
 import sys
 import sqlite3
@@ -35,50 +35,69 @@ def is_image(name):
 
 class MyWidget(QMainWindow):
     def __init__(self):
+        self.stopped = True
         super().__init__()
         uic.loadUi('simple_gui.ui', self)  # Загружаем дизайн
         self.pushButton.clicked.connect(self.run)
-        # Обратите внимание: имя элемента такое же как в QTDesigner
+        self.pushButton_2.clicked.connect(self.stop)
 
     def run(self):
+        self.stopped = False
 
         ALL_DATA = {}
         text = f''
         hashed_data = []
+        print('Начало поиска файлов')
         for j in (os.walk("D:\\")):
-            for k in j[2]:
-                if is_image(k):
-                    ALL_DATA[k] = j[0]
-                    path = j[0] + '\\' + k
-                    query = f"""select count(1) as cnt from graphical_files where file_name = '{k}' and path='{path}'"""
-                    result = cur.execute(query)
-                    text += f'{k} {path}\n'
-                    self.plainTextEdit.setPlainText(text)
-                    if list(result)[0][0] == 0:
-                        f = open(path, 'rb')
-                        img = f.read()
-                        x = hashlib.md5(img).hexdigest()
-                        hashed_data.append(x)
-                        query = f"""INSERT INTO graphical_files VALUES ('{k}', '{path}', '{x}')"""
-                        cur.execute(query)
+            if not self.stopped:
+                for k in j[2]:
+                    if is_image(k):
+                        ALL_DATA[k] = j[0]
+                        path = j[0] + '\\' + k
+                        query = f"""select count(1) as cnt from graphical_files where file_name = '{k}' and path='{path}'"""
+                        result = cur.execute(query)
+                        text += f'{k} {path}\n'
+                        if list(result)[0][0] == 0:
+                            f = open(path, 'rb')
+                            img = f.read()
+                            x = hashlib.md5(img).hexdigest()
+                            hashed_data.append(x)
+                            query = f"""INSERT INTO graphical_files VALUES ('{k}', '{path}', '{x}')"""
+                            cur.execute(query)
+        con.commit()
+        self.plainTextEdit.setPlainText(text)
+        print('Конец поиска файлов')
         data = []
+        print('Поиск хэшей')
         for i in set(hashed_data):
             if hashed_data.count(i) > 1:
                 query = f"""select file_name, path from graphical_files where (file_hash = '{i}')"""
                 data.append(list(cur.execute(query)))
-        print(data)
+        print('Конец поиска хэшей')
+        text_2 = f''
         for i in range(len(data)):
-            # self.label_2.setText(f'{data[0][i][0]}, {data[0][i][1]}')
-            pass
+            for j in range(len(data[i])):
+                text_2 += f'{data[i][j][0]} {data[i][j][1]}'
+
+        self.plainTextEdit_2.setPlainText(text_2)
 
     def closeEvent(self, event):  # Переопределяем кнопку закрытия приложения
-        text, ok_pressed = QInputDialog.getText(
-            self, 'Вы покидаете приложение', 'Вы уверены что хотите уйти?(y/n)')
-        if ok_pressed and text.lower() == 'y':
-            # print(text)
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle("Выход")
+        dlg.setText("Вы уверены, что хотите уйти?")
+        dlg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        dlg.setIcon(QMessageBox.Question)
+        button = dlg.exec()
+
+        if button == QMessageBox.Yes:
             event.accept()
+            con.close()
+            os.remove('files.db')
         else:
             event.ignore()
+
+    def stop(self):
+        self.stopped = True
 
 
 if __name__ == '__main__':
@@ -86,5 +105,3 @@ if __name__ == '__main__':
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
-    con.commit()
-    con.close()
